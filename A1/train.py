86de2c82a0faabd7c79dc4b0804f1763724af736
ztex <ztex030640417@gmail.com>
2018-10-07 22:42:31 +0800

    Build up the model

--- A1/train.py
+++ A1/train.py
@@ -6,7 +6,7 @@ validation_filename = './validation.tfrecords'
 class CNN():
     def __init__():
         pass
-        
+
     def weight_variable(shape):
         initial = tf.truncated_normal(shape, stddev=0.1)
         return tf.Variable(initial)
@@ -23,6 +23,45 @@ class CNN():
     def max_pool_2x2(x):
         # stride [1, x_movement, y_movement, 1]
         return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+    def build():
+        # define placeholder for inputs to network
+        self.xs = tf.placeholder(tf.float32, [None, 784])/255.   # 28x28
+        self.ys = tf.placeholder(tf.float32, [None, 10])
+        self.keep_prob = tf.placeholder(tf.float32)
+        x_image = tf.reshape(self.xs, [-1, 28, 28, 1])
+        # print(x_image.shape)  # [n_samples, 28,28,1]
+
+        ## conv1 layer ##
+        W_conv1 = self.weight_variable([5,5, 1,32]) # patch 5x5, in size 1, out size 32
+        b_conv1 = self.bias_variable([32])
+        h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1) # output size 28x28x32
+        h_pool1 = max_pool_2x2(h_conv1)                                         # output size 14x14x32
+
+        ## conv2 layer ##
+        W_conv2 = self.weight_variable([5,5, 32, 64]) # patch 5x5, in size 32, out size 64
+        b_conv2 = self.bias_variable([64])
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2) # output size 14x14x64
+        h_pool2 = max_pool_2x2(h_conv2)                                         # output size 7x7x64
+
+        ## fc1 layer ##
+        W_fc1 = self.weight_variable([7*7*64, 1024])
+        b_fc1 = self.bias_variable([1024])
+        # [n_samples, 7, 7, 64] ->> [n_samples, 7*7*64]
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+        h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+        ## fc2 layer ##
+        W_fc2 = self.weight_variable([1024, 10])
+        b_fc2 = self.bias_variable([10])
+        self.prediction = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+
+        # the error between prediction and real data
+        self.cross_entropy = tf.reduce_mean(-tf.reduce_sum(self.ys * tf.log(self.prediction),
+                                                    reduction_indices=[1]))       # loss
+        self.train_step = tf.train.AdamOptimizer(1e-4).minimize(self.cross_entropy)
 
 
 
