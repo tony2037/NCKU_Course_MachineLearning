8fc1bc0c36b38b45b736794c85ef7651e2d99245
ztex <ztex030640417@gmail.com>
2018-10-19 14:11:31 +0800

    NoP

--- A1/train.py
+++ A1/train.py
@@ -8,34 +8,40 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # See https://stackoverflow.com/questio
 train_filename = './train.tfrecords'
 validation_filename = './validation.tfrecords'
 
+train_set_path = './training/'
+valid_set_path = './validation/'
+
 class Dataset():
-    def __init__(train_path, validation_path):
+    def __init__(self, train_path, validation_path):
         self.filesnames = []
         self.labels = []
-        for i in range(sample_num):
-            images = glob('%sSample%s/*.png' % (data_path, str(i+1).zfill(3)))
+        for i in range(10):
+            images = glob('%sSample%s/*.png' % (train_path, str(i+1).zfill(3)))
             label = [0]* 10
             label[i] = 1
             for image in images:
                 self.filesnames.append(image)
                 self.labels.append(label)
 
-        self.filenames = tf.constant(self.filenames)
+        self.filesnames = tf.constant(self.filesnames)
         self.labels = tf.constant(self.labels)
-        self.dataset = tf.data.Dataset.from_tensor_slices((self.filenames, self.labels))
+        self.dataset = tf.data.Dataset.from_tensor_slices((self.filesnames, self.labels))
         self.dataset = self.dataset.map(self._parse_function)
-        self.dataset = self.dataset.shuffle(buffersize=1000).batch(32).repeat(100)
-    def _parse_function(filename, label):
+        self.dataset = self.dataset.shuffle(buffer_size=1000).batch(32).repeat(100)
+
+        self.iterator = self.dataset.make_one_shot_iterator()
+    def _parse_function(self, filename, label):
         image_string = tf.read_file(filename)
-        image_decoded = tf.image.decode_image(image_string)
+        image_decoded = tf.image.decode_png(image_string)
         image_resized = tf.image.resize_images(image_decoded, [28, 28])
         return image_resized, label
 
 
 class CNN():
-    def __init__(self, train_filename_queue, validation_filename_queue):
+    def __init__(self, train_filename_queue, validation_filename_queue, dataset):
         self.train_filename_queue = train_filename_queue
         self.validation_filename_queue = validation_filename_queue
+        self.dataset = dataset
 
     def weight_variable(self, shape):
         initial = tf.truncated_normal(shape, stddev=0.1)
@@ -60,17 +66,16 @@ class CNN():
         slim.model_analyzer.analyze_vars(model_vars, print_info=True)
 
     def build(self):
+        features, labels = self.dataset.iterator.get_next()
+        features = tf.cast(features, tf.float32)
+        labels = tf.cast(labels, tf.float32)
         # define placeholder for inputs to network
-        self.xs = tf.placeholder(tf.float32, [None, 784])/255.   # 28x28
-        self.ys = tf.placeholder(tf.float32, [None, 10])
         self.keep_prob = tf.placeholder(tf.float32)
-        x_image = tf.reshape(self.xs, [-1, 28, 28, 1])
-        # print(x_image.shape)  # [n_samples, 28,28,1]
 
         ## conv1 layer ##
         W_conv1 = self.weight_variable([5,5, 1,32]) # patch 5x5, in size 1, out size 32
         b_conv1 = self.bias_variable([32])
-        h_conv1 = tf.nn.relu(self.conv2d(x_image, W_conv1) + b_conv1) # output size 28x28x32
+        h_conv1 = tf.nn.relu(self.conv2d(features, W_conv1) + b_conv1) # output size 28x28x32
         h_pool1 = self.max_pool_2x2(h_conv1)                                         # output size 14x14x32
 
         ## conv2 layer ##
@@ -85,7 +90,7 @@ class CNN():
         # [n_samples, 7, 7, 64] ->> [n_samples, 7*7*64]
         h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-        h_fc1_drop = tf.nn.dropout(h_fc1, self.keep_prob)
+        h_fc1_drop = tf.nn.dropout(h_fc1, 0.5)
 
         ## fc2 layer ##
         W_fc2 = self.weight_variable([1024, 10])
@@ -95,12 +100,18 @@ class CNN():
         print(self.prediction.shape)
 
         # the error between prediction and real data
-        self.cross_entropy = tf.reduce_mean(-tf.reduce_sum(self.ys * tf.log(self.prediction),
+        self.cross_entropy = tf.reduce_mean(-tf.reduce_sum(labels * tf.log(self.prediction),
                                                     reduction_indices=[1]))       # loss
         self.train_step = tf.train.AdamOptimizer(1e-4).minimize(self.cross_entropy)
 
         # The model information
         self.model_summary()
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            for i in range(100):
+                _, loss_value = sess.run([self.train_step, self.cross_entropy])
+                print("Iter: {}, Loss: {:.4f}".format(i, loss_value))
 
     def train(self,epochs):
         sess = tf.Session()
@@ -145,11 +156,15 @@ class CNN():
         return image, label
 
 if __name__ == '__main__':
+
+    # Construct a Dataset object
+    Dataset = Dataset(train_set_path, valid_set_path)
+
     train_filename_queue = tf.train.string_input_producer([train_filename],num_epochs=None) # read in the stream
     validation_filename_queue = tf.train.string_input_producer([validation_filename],num_epochs=None) # read in the stream
     
     print('Build up model')
-    model = CNN(train_filename_queue = train_filename_queue, validation_filename_queue = validation_filename_queue)
+    model = CNN(train_filename_queue = train_filename_queue, validation_filename_queue = validation_filename_queue, dataset = Dataset)
     model.build()
 
     print('Data proccess')
@@ -160,5 +175,4 @@ if __name__ == '__main__':
 
     filename_queue = tf.train.string_input_producer([validation_filename],num_epochs=None) # read in the stream
     valid_image, valid_label = model.decode_from_tfrecords(filename_queue, is_batch=True)
-    
-    
+
